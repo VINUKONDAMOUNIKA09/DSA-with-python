5280fae2a4cdfd73d40aa1cee4038ae76f15db59
VINUKONDAMOUNIKA09 <mm7039222@gmail.com>
2025-07-01 12:17:43 +0530

    Add files via upload

---
+++ array operations.py
@@ -0,0 +1,18 @@
+'''Aray operations
+#program to consider a list arr=[10,20,30,40] and perform insert operation with 50 and 25 at position 2 respectively, deleted 30 and traverse the array to fetch a number 25 is present or not'''
+arr=[10,20,30,40]
+#insert
+arr.append(50)
+arr.insert(2,25)
+#print(arr)
+for i in arr:
+    print(i, end=' ')
+#deletion
+arr.remove(30)
+arr.pop()
+#print(arr)
+#traversal
+for i in arr:
+    print(i, end=' ')
+#searching
+print("\n 25 in array?" ,25 in arr)
